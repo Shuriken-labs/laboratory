d78b4b2e8016efdace6969cbf97d5318bd6753cd
inspi-writer <conservedinnovation@gmail.com>
2023-09-15 23:01:43 +0100

    some for walletconnect

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,6 +4,8 @@ import { isDesktop, isMobile } from "../helpers/Responsiveness";
 import shurikenLogo from "../assets/Shuriken Labs-08.png";
 import WalletState from "../utils/State";
 import { useNavigate } from "react-router-dom";
+import { Web3Button } from "@web3modal/react";
+import { useWeb3Modal } from "@web3modal/react";
 
 // console.log(window.ethereum?.selectedAddress);
 
@@ -16,6 +18,7 @@ const Navbar = () => {
     wallet
   } = useContext(WalletState);
   const navigate = useNavigate();
+  const { open, close } = useWeb3Modal();
 
   return (
     <div className="__nav_box min-w-full fixed max_screen z-20">
@@ -41,6 +44,20 @@ const Navbar = () => {
                   8
                 )}...${wallet.walletAddress.substr(-8, 8)}`
               : "connect wallet"}
+          </button>
+        </div>
+        <div className="__connect_container theta flex">
+          <button
+            className="btn 
+           btn-light btn-sm __btn_white"
+            onClick={() => open()}
+          >
+            {wallet.walletAddress.length > 2
+              ? `${wallet.walletAddress.substr(
+                  0,
+                  8
+                )}...${wallet.walletAddress.substr(-8, 8)}`
+              : "wallet Connect"}
           </button>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -14,18 +14,87 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { formatEther } from "@ethersproject/units";
 import { MetaMask } from "@web3-react/metamask";
 
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+const ottochain = {
+  id: 42161,
+  name: "Arbitrum One",
+  network: "arbitrum",
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18
+  },
+  rpcUrls: {
+    alchemy: {
+      http: ["https://arb-mainnet.g.alchemy.com/v2"],
+      webSocket: ["wss://arb-mainnet.g.alchemy.com/v2"]
+    },
+    infura: {
+      http: ["https://arbitrum-mainnet.infura.io/v3"],
+      webSocket: ["wss://arbitrum-mainnet.infura.io/ws/v3"]
+    },
+    default: {
+      http: ["https://arb1.arbitrum.io/rpc"]
+    },
+    public: {
+      http: ["https://arb1.arbitrum.io/rpc"]
+    }
+  },
+  blockExplorers: {
+    etherscan: {
+      name: "Arbiscan",
+      url: "https://arbiscan.io"
+    },
+    default: {
+      name: "Arbiscan",
+      url: "https://arbiscan.io"
+    }
+  },
+  contracts: {
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      blockCreated: 7654707
+    }
+  }
+};
+
+const chains = [ottochain];
+const projectId = process.env.REACT_APP_WALLET_CONNECT_ID;
+console.log(projectId);
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Web3ReactProvider
-      getLibrary={(provider) => new Web3Provider(provider)}
-      connectors={[InjectedConnector]}
-    >
-      <WalletProvider>
-        <App />
-      </WalletProvider>
-    </Web3ReactProvider>
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <WagmiConfig config={wagmiConfig}>
+        <Web3ReactProvider
+          getLibrary={(provider) => new Web3Provider(provider)}
+          connectors={[InjectedConnector]}
+        >
+          <WalletProvider>
+            <App />
+          </WalletProvider>
+        </Web3ReactProvider>
+      </WagmiConfig>
+    </React.StrictMode>
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
